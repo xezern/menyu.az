7f7d1b730ed110bcdb9e4ee984fe873cad8c8ee0
xezern <xezernovruz79@gmail.com>
2025-07-19 13:18:06 +0400

    registerin legvi

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,7 +7,7 @@ const auth = require('../middlewares/auth.middleware');
 const validator = require('../middlewares/validation.middleware');
 
 const authSchema = require('../schema/auth.schema');
-router.post('/register', register);
+// router.post('/register', register);
 router.post('/login', validator(authSchema), login);
 router.get('/verify-token', auth, verifyToken);
 module.exports = router;
