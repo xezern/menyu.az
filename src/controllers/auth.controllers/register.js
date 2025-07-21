f3c9e652a8a7e7b4864d29eb830cc3f6f115cf37
xezern <xezernovruz79@gmail.com>
2025-07-21 04:45:51 +0400

    bcryptjs inteqrasiyasi

--- src/controllers/auth.controllers/register.js
+++ src/controllers/auth.controllers/register.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../../config/db'); // mysql2 bağlantısı
 
 const register = async (req, res) => {
