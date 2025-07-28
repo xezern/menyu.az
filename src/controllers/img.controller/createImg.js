f0deaac85139b5c9167f0a13ece461f235f4994f
xezern <xezernovruz79@gmail.com>
2025-07-28 20:11:08 +0400

    İmage optimizator

--- src/controllers/img.controller/createImg.js
+++ src/controllers/img.controller/createImg.js
@@ -1,28 +1,55 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+
 function createImg(req, res) {
     if (req.file.fieldname !== 'img') {
         res.status(400).json({
             message: `Sizden gözlənilən field name: "img", Sizin göndərdiyiniz filed name: ${req.file.fieldname} `,
         });
-        return
+        return;
     }
 
     try {
-        const filePath = `/uploads/${req.file.filename.replaceAll(" ", '')}`;
-        res.status(201).json({
-            message: 'Şəkil uğurla yükləndi',
-            file: {
-                originalName: req.file.originalname,
-                fileName: req.file.filename,
-                mimeType: req.file.mimetype,
-                size: req.file.size,
-                path: filePath
-            }
-        });
+        // Şəkilin orijinal yeri
+        const inputPath = path.join(__dirname, '..', '..', '..', 'uploads', req.file.filename);
+
+        // Şəkilin optimallaşdırılmış yeri
+        const optimizedFileName = `${Date.now()}-${req.file.filename.replaceAll(" ", '')}`;
+        const outputPath = path.join(__dirname, '..', '..', '..', 'uploads', optimizedFileName);
+
+        // Şəkli optimallaşdırırıq
+        sharp(inputPath)
+            .resize(800) // Maksimum 800px genişlik
+            .jpeg({ quality: 90 }) // JPEG formatında keyfiyyətini 80 edir
+            .toFile(outputPath, (err, info) => {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Şəkil optimallaşdırıla bilmədi',
+                    });
+                }
+
+                fs.unlinkSync(inputPath);
+
+                // Optimallaşdırılmış şəkilin serverə saxlanılması
+                res.status(201).json({
+                    message: 'Şəkil uğurla yükləndi və optimallaşdırıldı',
+                    file: {
+                        originalName: req.file.originalname,
+                        fileName: optimizedFileName,
+                        mimeType: req.file.mimetype,
+                        size: info.size,
+                        path: `/uploads/${optimizedFileName}`
+                    }
+                });
+            });
     } catch (err) {
+        console.log(err);
+        
         res.status(500).json({
             message: err.errors ? err.errors : 'Internal Server Error',
         });
     }
 }
 
-module.exports = createImg;
+module.exports = createImg
